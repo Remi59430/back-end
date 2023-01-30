fed5bf25661c7a9fc09aa410d3859eaf67a565d6
Remi59430 <remipollefliet@gmail.com>
2023-01-30 09:07:35 +0100

    first

---
+++ php/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+    <!-- Head -->
+    <?php
+        include("./includes/head.html");
+    ?>
+
+    <!-- Header -->
+    <?php
+        include("./includes/header.inc.html");
+    ?>
+
+    <!-- Navigation Bar -->
+    <?php
+        include("./includes/nav.inc.html");
+    ?>
+
+    <!-- Section -->
+    <?php
+
+    ?>
+
+    <!-- Footer -->
+    <?php
+        include("./includes/footer.inc.html");
+    ?>
+
+</html>

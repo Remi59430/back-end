0a259a8c3811ba18954fba9a1b02379d62bdb9e9
Remi59430 <remipollefliet@gmail.com>
2023-01-30 11:19:52 +0100

    upgrade nav

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@
 
     <!-- Head -->
     <?php
-        include("./includes/head.html");
+        include("./includes/head.inc.html");
     ?>
 
     <!-- Header -->
@@ -12,19 +12,55 @@
         include("./includes/header.inc.html");
     ?>
 
-    <!-- Navigation Bar -->
-    <?php
-        include("./includes/nav.inc.html");
-    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col col-md-2 col-sm-1">
+                <!-- Navigation Bar -->
+                <?php
+                    include("./includes/nav.inc.html");
+                ?>
 
-    <!-- Section -->
-    <?php
+                <!-- Section -->
+                <?php
 
-    ?>
+                    if(isset($_GET["Debogage"])){
+                        include("./pages/Debogage.html");
+                    }
 
-    <!-- Footer -->
-    <?php
-        include("./includes/footer.inc.html");
-    ?>
+                    elseif(isset($_GET["Concatenation"])){
+                        include("./pages/Concatenation.html");
+                    }
+
+                    elseif(isset($_GET["Boucle"])){
+                        include("./pages/Boucle.html");
+                    }
+
+                    elseif(isset($_GET["Fonction"])){
+                        include("./pages/Fonction.html");
+                    }
+
+                    elseif(isset($_GET["Supprimer"])){
+                        include("./pages/Supprimer.html");
+                    }
+
+                    else{
+                        ?>
+                            <button type="button" class="btn btn-primary">Ajouter une valeur</button>
+                        <?php
+                    }
+
+                 ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+        <!-- Footer -->
+        <?php
+            include("./includes/footer.inc.html");
+        ?>
+        </div>
+    </div>
 
 </html>

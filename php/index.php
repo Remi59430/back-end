d4eba3bf1766d32981100d08ddaa1bce9cf4b205
Remi59430 <remipollefliet@gmail.com>
2023-02-01 15:29:44 +0100

    nearly finished

--- php/index.php
+++ php/index.php
@@ -24,7 +24,6 @@
 
     <div class="container-fluid">
         <div class="row">
-
             <!-- Navigation Bar -->
             <nav class="col-sm-4 col-xs-12">
                 <a href="index.php" class="list-group-item list-group-item-action">
@@ -53,96 +52,95 @@
                     
                         $_SESSION['table'] = $table;
                     }
-
-                    elseif(isset($_GET["Debogage"])){
-
-                        echo '<h2 class="d-flex justify-content-center">Débogage</h2>';
-                        echo '<h3 class="mt-4 mb-4">===>Lecture du tableau à l\'aide de la fonction print_r()</h3>';
-                        
-                        echo "<pre>";
-                        print_r($table);
-                        echo "</pre>";
-                        
-                    }
-
-                    elseif(isset($_GET["Concatenation"])){
-
-                        echo '<h2 class="d-flex justify-content-center">Concaténation</h2>';
-
-                        echo '<h3 class="mt-4 mb-4">===>Construction d\'une phrase avec le contenu du tableau</h3>';
-                        echo 'Mr ' .$table["first_name"] .' ' .$table["last_name"] ."<br>"; 
-                        echo 'J\'ai ' .$table["age"] .' ans et je mesure ' .$table["size"] .' m.';
-                        
-                        $table['first_name'] = ucfirst($table['first_name']);
-                        $table['last_name'] = strtoupper($table['last_name']);
-                   
-                        echo '<h3 class="mt-4 mb-4">===>Construction d\'une phrase après MAJ du tableau</h3>';
-                        echo 'Mr ' .$table['first_name'] .' ' .$table['last_name'] ."<br>";
-                        echo 'J\'ai ' .$table["age"] .' ans et je mesure ' .$table["size"] .' m.';
-
-                        $table['size'] = str_replace('.', ',', $table['size']);
-
-                        echo '<h3 class="mt-4 mb-4">===>Affichage d\'une virgule à la place du point pour la taille</h3>';
-                        echo 'Mr ' .$table['first_name'] .' ' .$table['last_name'] ."<br>";
-                        echo 'J\'ai ' .$table['age'] .' ans et je mesure ' .$table['size'] .' m.';
-                    }   
-
-                    elseif(isset($_GET["Boucle"])){
-                        echo '<h2 class="d-flex justify-content-center">Boucle</h2>';
-
-                        echo '<h3 class="mt-4 mb-4">===>Lecture du tableau à l\'aide d\'une boucle foreach</h3>';
-
-                        $number=0;
-
-                        foreach($table as $key => $value){
-                            echo 'à la ligne n°' .$number .' correspond la clé "' .$key .'" et contient "' .$value .'"' .'<br>';
-                            $number++;
-                        }
-                    }
-
-                    elseif(isset($_GET["Fonction"])){
-                        echo '<h2 class="d-flex justify-content-center">Fonction</h2>';
-                        echo '<h3 class="mt-4 mb-4">===>J\'utilise ma fonction readTable()</h3>';
-
-                        function readTable($table){
-                            $number=0;
-                            foreach($table as $key => $value){
-                                echo 'à la ligne n°' .$number .' correspond la clé "' .$key .'" et contient "' .$value .'"' .'<br>';
-                                $number++;
-                            }
-                        }
-
-                        readTable($table);
-                    }
-                        
-
-                    elseif(isset($_GET["Supprimer"])){
-                        echo '<div class="alert alert-success justify-content-center d-flex">Données supprimées</div>';
-                    
-                        session_destroy();
-                }
-                        
+    
                     elseif(isset($_GET["add"])){
                         echo '<h2 class="d-flex justify-content-center">Ajouter des données</h2>';
                         include("./includes/form.inc.html");
                     }
 
                     else{
-                ?>
-                        <div>
-                            <a href="index.php?add">
-                                <button type="button" class="btn btn-primary">Ajouter des données</button>
-                            </a>
-                        </div>
-                <?php
+                        if(isset($table)){
+                            if(isset($_GET["Debogage"])){
+                                echo '<h2 class="d-flex justify-content-center">Débogage</h2>';
+                                echo '<h3 class="mt-4 mb-4">===>Lecture du tableau à l\'aide de la fonction print_r()</h3>';
+                                
+                                echo "<pre>";
+                                print_r($table);
+                                echo "</pre>";
+                            }
+        
+                            elseif(isset($_GET["Concatenation"])){
+                                echo '<h2 class="d-flex justify-content-center">Concaténation</h2>';
+        
+                                echo '<h3 class="mt-4 mb-4">===>Construction d\'une phrase avec le contenu du tableau</h3>';
+                                echo 'Mr ' .$table["first_name"] .' ' .$table["last_name"] ."<br>"; 
+                                echo 'J\'ai ' .$table["age"] .' ans et je mesure ' .$table["size"] .' m.';
+                                
+                                $table['first_name'] = ucfirst($table['first_name']);
+                                $table['last_name'] = strtoupper($table['last_name']);
+                           
+                                echo '<h3 class="mt-4 mb-4">===>Construction d\'une phrase après MAJ du tableau</h3>';
+                                echo 'Mr ' .$table['first_name'] .' ' .$table['last_name'] ."<br>";
+                                echo 'J\'ai ' .$table["age"] .' ans et je mesure ' .$table["size"] .' m.';
+        
+                                $table['size'] = str_replace('.', ',', $table['size']);
+        
+                                echo '<h3 class="mt-4 mb-4">===>Affichage d\'une virgule à la place du point pour la taille</h3>';
+                                echo 'Mr ' .$table['first_name'] .' ' .$table['last_name'] ."<br>";
+                                echo 'J\'ai ' .$table['age'] .' ans et je mesure ' .$table['size'] .' m.';
+                            }   
+        
+                            elseif(isset($_GET["Boucle"])){
+                                echo '<h2 class="d-flex justify-content-center">Boucle</h2>';
+        
+                                echo '<h3 class="mt-4 mb-4">===>Lecture du tableau à l\'aide d\'une boucle foreach</h3>';
+        
+                                $number=0;
+        
+                                foreach($table as $key => $value){
+                                    echo 'à la ligne n°' .$number .' correspond la clé "' .$key .'" et contient "' .$value .'"' .'<br>';
+                                    $number++;
+                                }
+                            }
+        
+                            elseif(isset($_GET["Fonction"])){
+                                echo '<h2 class="d-flex justify-content-center">Fonction</h2>';
+                                echo '<h3 class="mt-4 mb-4">===>J\'utilise ma fonction readTable()</h3>';
+        
+                                function readTable($table){
+                                    $number=0;
+                                    foreach($table as $key => $value){
+                                        echo 'à la ligne n°' .$number .' correspond la clé "' .$key .'" et contient "' .$value .'"' .'<br>';
+                                        $number++;
+                                    }
+                                }
+        
+                                readTable($table);
+                            }
+                                
+                            elseif(isset($_GET["Supprimer"])){
+                                echo '<div class="alert alert-success justify-content-center d-flex">Données supprimées</div>';
+                            
+                                session_destroy();
+                            }
+                            
+                            else{
+                            echo '<div><a href="index.php?add"><button type="button" class="btn btn-primary">Ajouter des données</button></a></div>';
+                            }
+                        }
+                        
+                        else{
+                        echo '<div><a href="index.php?add"><button type="button" class="btn btn-primary">Ajouter des données</button></a></div>';
+                        }
                     }
                 ?>
+
             </div>   
         </div>
     </div>
     <div class="row">
             <div class="col d-flex justify-content-center">
-                
+
             <!-- Footer -->
             <?php
                 include("./includes/footer.inc.html");
